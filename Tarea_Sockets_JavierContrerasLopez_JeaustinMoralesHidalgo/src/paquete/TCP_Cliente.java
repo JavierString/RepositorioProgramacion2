072f3f8fece9aa3f6c2269108ea5f9fbfba5cd4b
Javier Contreras <javiercl180906@gmail.com>
2025-05-12 22:30:54 -0600

    cambio javier 3312

---
+++ Tarea_Sockets_JavierContrerasLopez_JeaustinMoralesHidalgo/src/paquete/TCP_Cliente.java
@@ -0,0 +1,50 @@
+package Tarea_Sockets_JavierContrerasLopez_JeaustinMoralesHidalgo.src.paquete;
+import java.io.*;
+import java.net.*;
+import javax.swing.*;
+
+public class TCP_Cliente {
+
+    /*TCP de parte del cliente, adaptado en interfaz gráfica por JOptionPane para la entreda de datos 
+      
+      El código fuente antes devolvia un String del usuario convertido a mayúsculas, este ahora devuelve un-
+      número decimal enviado por el usuario y el server le devuelve el numero convertido en notación binaria 
+     
+    */
+
+
+     public static void main(String[] args){
+        //variable String para la entrada de datos, en el cual tiene que ingresar un numero 
+         String entrada = JOptionPane.showInputDialog(null, "Ingrese un número (o 'salir'):", "Cliente TCP", JOptionPane.QUESTION_MESSAGE);
+
+       
+         /* Este bloque de código verifica si la entrada es nula o si se escribe "salir" sin importar mayusculas o minusculas-
+          Si es así entonces sale el mensaje de cliente finalizado */
+        if (entrada == null || entrada.equalsIgnoreCase("salir")) {
+            JOptionPane.showMessageDialog(null, "Cliente finalizado.");
+            return;
+        }
+
+        try {
+            //se crea un objeto cliente de tipo Socker con su hostname y en el puerto 7777
+            Socket clientSocket = new Socket("hostname", 7777);
+            DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+            BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+            outToServer.writeBytes(entrada + "\n");
+            String respuestaServidor = inFromServer.readLine();
+
+            JOptionPane.showMessageDialog(null, "Respuesta del servidor: " + respuestaServidor);
+
+            //finaliza la conexión del cliente mediante este metodo
+            clientSocket.close();
+        } catch (IOException excepcion) {
+            JOptionPane.showMessageDialog(null, "Error de conexión: " + excepcion.getMessage());
+ }
+
+
+
+ }
+
+
+}
